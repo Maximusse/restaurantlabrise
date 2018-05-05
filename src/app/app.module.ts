a6c98fe1d233535676968195cfe2e303484e2d41
Maximusse <dekomaximusse@gmail.com>
2018-05-05 18:56:19 +0000

    Nouveau site de la brise
    Premier déploement.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserXhr  } from '@angular/http';
+import { NgProgressModule, NgProgressBrowserXhr  } from 'ngx-progressbar';
+import { Routes, RouterModule } from '@angular/router';
+import { ScrollToModule } from 'ng2-scroll-to-el';
+
+import { routes } from './app.router';
+
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    routes,
+    NgProgressModule,
+    ScrollToModule.forRoot()
   ],
-  providers: [],
+  providers: [
+    {provide: BrowserXhr, useClass: NgProgressBrowserXhr}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+
+declare var $: any;
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+
+    $(function(){
+      $('.button-collapse').sideNav();
+
+      $(window).scroll(function(){
+        let posScroll = $(document).scrollTop();
+          if(posScroll >= 250){ 
+            $('#navbar-two').fadeIn();
+          }else {
+            $('#navbar-two').fadeOut();
+          }
+      });
+
+      
+
+    })
+
+  }
+
+}
